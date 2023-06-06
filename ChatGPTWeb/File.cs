c12080644907517ac3299e1467a548e0860763ee
Parashram.Rathod <Parashram.Rathod@KANINI-LTP-602>
2023-06-06 13:17:21 +0530

    Removed old proj and added new Project ChatGPTWeb app

--- ChatGPTWeb/File.cs
+++ ChatGPTWeb/File.cs
@@ -1,12 +1,1 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-
-namespace ChatGPTWeb
-{
-    public partial class File
-    {
-        public void OnChooseFile(InputFileChangeEventArgs e)
-        {
-
-        }
-    }
-}
+﻿
