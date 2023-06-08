0bf4be27b2b02c313cb6e916628f8580f367a438
Parashram.Rathod <Parashram.Rathod@KANINI-LTP-602>
2023-06-08 17:17:40 +0530

    updated the file upload changes with API

---
+++ ChatGPTAPI/Entities/FileDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatGPTAPI.Entities
+{
+    
+        [Table("FileDetails")]
+        public class FileDetails
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int ID { get; set; }
+            public string FileName { get; set; }
+            public byte[] FileData { get; set; }
+            public FileType FileType { get; set; }
+        }
+    
+}
